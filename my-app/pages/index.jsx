7c908f24159b7b7cc68988c933c33113cc812155
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-04-25 12:10:49 +0530

    routing & dynamic route parameters

---
+++ my-app/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>Index Page</div>
+  )
+}
+
+export default Page;
---
+++ my-app/pages/notes/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>Note index Path</div>
+  )
+}
+
+export default Page;
