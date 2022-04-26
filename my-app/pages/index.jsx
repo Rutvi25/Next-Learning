f0b267a631253c67a9fdf81d459abeb1e90cec5d
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-04-26 09:03:13 +0530

    css modules

--- my-app/pages/index.jsx
+++ my-app/pages/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 const Page = () => {
-  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
+  // const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
   return (
     <div>
       <h3>Index page</h3>
--- my-app/pages/notes/index.jsx
+++ my-app/pages/notes/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../src/styles.module.css'
 
 const Page = () => {
   return (
