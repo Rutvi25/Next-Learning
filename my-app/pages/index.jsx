c96e3e9a89ec4cdd4728fc75f2b6c88d79db09fa
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-04-26 11:17:21 +0530

    styling with theme-ui

--- my-app/pages/index.jsx
+++ my-app/pages/index.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+import '../src/styles.module.css';
 
 const Page = () => {
-  // const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
   return (
     <div>
-      <h3>Index page</h3>
-      {notes.map(note => (
-        <div>
-          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
-            <a>
-              <strong>{note.title}</strong>
-            </a>
-          </Link>
-        </div>
-      ))}
+      <h1>Index page</h1>
+      <Link href={'/notes'}>
+        <a>Link</a>
+      </Link>
     </div>
   )
 }
--- my-app/pages/notes/index.jsx
+++ my-app/pages/notes/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import '../src/styles.module.css'
+import Link from 'next/link';
+import '../../src/styles.module.css';
 
 const Page = () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
   return (
-    <div>Note index Path</div>
+    <div>
+      <h3>Note index Path</h3>
+      {notes.map(note => (
+          <div>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a>
+                <strong>{note.title}</strong>
+              </a>
+            </Link>
+          </div>
+        ))}
+    </div>
   )
 }
 
