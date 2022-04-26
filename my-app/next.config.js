8ced87fff58a1fc219d9e348391aa2904c642358
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-04-26 18:03:24 +0530

    plugins

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,9 +1,17 @@
-const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+// const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = (phase, { defaultConfig }) => {
-  if(phase = PHASE_DEVELOPMENT_SERVER) {
-    console.log('I\'m in dev mode');
-    return defaultConfig
-  }
-  return defaultConfig
-}
+// module.exports = (phase, { defaultConfig }) => {
+//   if(phase = PHASE_DEVELOPMENT_SERVER) {
+//     console.log('I\'m in dev mode');
+//     return defaultConfig
+//   }
+//   return defaultConfig
+// }
+
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+
+dotenvLoad()
+
+const withEnv = nextEnv()
+module.exports = withEnv()
