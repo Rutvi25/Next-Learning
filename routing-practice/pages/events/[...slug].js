ac366d8ddf5ddc8a3291f2e7fafb324c7cf570b8
Rutvi Patel <rutvipatel2500@gmail.com>
2022-06-30 23:02:32 +0530

    wip: routing practice project

---
+++ routing-practice/pages/events/[...slug].js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>
+        Filtered Events Page
+      </h1>
+    </div>
+  )
+}
+
+export default FilteredEventsPage
---
+++ routing-practice/pages/events/[eventId].js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EventDetailsPage = () => {
+  return (
+    <div>
+      <h1>
+        Event Details Page
+      </h1>
+    </div>
+  )
+}
+
+export default EventDetailsPage
