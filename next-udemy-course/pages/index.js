ba3ca1ac62647dced2ac9cc566f9887f99bfa9b4
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-01 12:29:39 +0530

    getStaticProps

--- next-udemy-course/pages/index.js
+++ next-udemy-course/pages/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HomePage = () => {
+const HomePage = (props) => {
+  const { products } = props;
   return (
     <div>
       <h1>Home Page</h1>
       <ul>
-        <li>
-          <Link href='/portfolio'>Portfolio</Link>
-        </li>
-        <li>
-          <Link href='/client'>Clients</Link>
-        </li>
+        <li><Link href='/portfolio'>Portfolio</Link></li>
+        <li><Link href='/client'>Clients</Link></li>
+      </ul>
+      {/*fetching this product data will be done on the server side, not the client side*/}
+      <ul>
+        {products.map(product => <li key={product.id}>{product.title}</li>)}
       </ul>
     </div>
   )
+}
+// getStaticProps function will be executed first and then the HomePage component will be executed/rendered.
+export async function getStaticProps() {
+  return { 
+    props: {
+      products: [{ id: 'p1', title: 'Product 1'}]
+    }
+  };
 }
 
 export default HomePage
