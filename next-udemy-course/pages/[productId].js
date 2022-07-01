027ed600685e866ba5e9400d2cc5feebb26b3090
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-01 18:50:17 +0530

    getStaticPaths & fallback

---
+++ next-udemy-course/pages/[productId].js
@@ -0,0 +1,43 @@
+import React from 'react';
+import fs from 'fs/promises';
+import path from 'path';
+import { title } from 'process';
+
+const ProductDetailsPage = ({ loadedProduct }) => {
+  // const { loadedProduct } = prpos;
+  // if(!loadedProduct) {
+  //   return <p>Loading...</p>
+  // }
+  return (
+    <div>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.productId;
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  const product = data.products.find(product => product.id === productId);
+  return {
+    props: {
+      loadedProduct: product
+    }
+  }
+}
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { productId: 'p1' } },
+      // { params: { productId: 'p2' } },
+      // { params: { productId: 'p3' } },
+    ],
+    fallback: "blocking"
+  }
+}
+
+export default ProductDetailsPage
--- next-udemy-course/pages/index.js
+++ next-udemy-course/pages/index.js
@@ -1,8 +1,8 @@
-import path from 'path';
 import React from 'react';
 import Link from 'next/link';
+import path from 'path';
 //importing file system module from node, 
-import fs from 'fs/promises'  
+import fs from 'fs/promises';
 // browser side javaScript can't access the file system, but it can be used inside the getStaticProps.
 // readFile() will return the promise when we use fs/promise.
 
@@ -16,23 +16,33 @@ const HomePage = (props) => {
         <li><Link href='/client'>Clients</Link></li>
       </ul>
       <ul>
-        {products.map(product => <li key={product.id}>{product.title}</li>)}
+        {products.map(product => <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>)}
       </ul>
     </div>
-  )
+  );
 }
 // getStaticProps function will be executed first and then the HomePage component will be executed/rendered.
 // fetching this product data will be done on the server side, not the client side
 export async function getStaticProps() {
-  console.log('Regenerating...')
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
-  const jsonData = await fs.readFile(filePath)
-  const data = JSON.parse(jsonData)
+  console.log('Regenerating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  if(!data) {
+    return {
+      redirect: {
+        destination: '/client'
+      }
+    }
+  }
+  if(data.products.length === 0) {
+    return { notFound: true };      //notFound key is used to return 404 error page
+  }
   return { 
     props: {
       products: data.products
     },
-    revalidate: 10
+    revalidate: 10,
   };
 }
 
