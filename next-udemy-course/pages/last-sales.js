eb4e3f133edf07f15b4ca6a36546484aa8dcdb2c
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-04 16:51:46 +0530

    pre-fetching with client side fetching

--- next-udemy-course/pages/last-sales.js
+++ next-udemy-course/pages/last-sales.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-const LastSalesPage = () => {
-  const [sales, setSales] = useState();
-  
-  const { data, error } = useSWR('https://nextjs-udemy-course-8ef7f-default-rtdb.firebaseio.com/sales.json');
+const LastSalesPage = (props) => {
+  const [sales, setSales] = useState(props.sales);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+  const { data, error } = useSWR(
+    'https://nextjs-udemy-course-8ef7f-default-rtdb.firebaseio.com/sales.json', fetcher
+  );
+
   useEffect(() => {
     if(data) {
       const transformedSales = [];
@@ -13,21 +17,45 @@ const LastSalesPage = () => {
           id: key, 
           username: data[key].username, 
           volume: data[key].volume 
-        })
+        });
       }
       setSales(transformedSales);
     }
-  }, [data])
+  }, [data]);
 
-  if(!error) {
+  if(error) {
     return <p>Failed to load</p>
   }
-  if(!data || !sales) {
+  if(!data && !sales) {
     return <p>Loading...</p>
   }
-  return <ul>
-    {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
-  </ul>
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 }
-
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://nextjs-udemy-course-8ef7f-default-rtdb.firebaseio.com/sales.json'
+  )
+  const data = await res.json()
+  const transformedSales = [];
+  for (const key in data) {
+    transformedSales.push({ 
+      id: key, 
+      username: data[key].username, 
+      volume: data[key].volume 
+    })
+  }
+  return {
+    props: {
+      sales: transformedSales
+    }, 
+  }
+}
 export default LastSalesPage
