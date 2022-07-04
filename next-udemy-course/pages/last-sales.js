8a7395dc73c148be0dd286aca31b2a20b0728325
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-04 15:39:10 +0530

    useSWR

--- next-udemy-course/pages/last-sales.js
+++ next-udemy-course/pages/last-sales.js
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 const LastSalesPage = () => {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
+  
+  const { data, error } = useSWR('https://nextjs-udemy-course-8ef7f-default-rtdb.firebaseio.com/sales.json');
   useEffect(() => {
-    setIsLoading(true)
-    fetch(
-      'https://nextjs-udemy-course-8ef7f-default-rtdb.firebaseio.com/sales.json'
-    ).then(res => res.json())
-      .then(data => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({ 
-            id: key, 
-            username: data[key].username, 
-            volume: data[key].volume 
-          })
-        }
-        setSales(transformedSales)
-        setIsLoading(false)
-      })
-  }, []);
-  if(isLoading) {
-    return <p>Loading...</p>
+    if(data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({ 
+          id: key, 
+          username: data[key].username, 
+          volume: data[key].volume 
+        })
+      }
+      setSales(transformedSales);
+    }
+  }, [data])
+
+  if(!error) {
+    return <p>Failed to load</p>
   }
-  if(!sales) {
-    return <p>No data available</p>
+  if(!data || !sales) {
+    return <p>Loading...</p>
   }
   return <ul>
     {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
