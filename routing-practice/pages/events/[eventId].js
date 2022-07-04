6c142277647bb8c0e5e9d1ccb23285fdb3e8c6a2
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-04 18:03:38 +0530

    wip: pre-rendering & data-fetching practice

--- routing-practice/pages/events/[eventId].js
+++ routing-practice/pages/events/[eventId].js
@@ -1,16 +1,16 @@
 import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
+function EventDetailPage(props) {
   const router = useRouter();
 
   const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -21,7 +21,7 @@ function EventDetailPage() {
   }
 
   return (
-    <Fragment>
+    <>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
@@ -32,8 +32,26 @@ function EventDetailPage() {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
-    </Fragment>
+    </>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+  return {
+    props: {
+      selectedEvent: event
+    }
+  }
+}
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map(event => ({ params: { eventId: event.id }}));
+  return {
+    paths: paths,
+    fallback: false
+  }
 }
 
 export default EventDetailPage;
