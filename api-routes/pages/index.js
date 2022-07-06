faa9a0d4b4b2c66dc1557d6100881b27224b9d80
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-07-06 16:16:51 +0530

    API routes

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,7 +1,55 @@
+import { useRef, useState } from 'react';
+
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([])
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  function submitFormHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = { email: enteredEmail, feedback: enteredFeedback }
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback)
+      })
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Email  </label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div><br />
+        <div>
+          <label htmlFor="feedback">Feedback  </label>
+          <textarea id="feedback" ref={feedbackInputRef}/>
+        </div><br />
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>
+        Load Feedback
+      </button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.feedback}</li>
+        ))}
+      </ul>
     </div>
   );
 }
